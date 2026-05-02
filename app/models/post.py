1467b68f0c31585aafe2184da84207944cf3fcdc
Ruzell <johnruzell123@gmail.com>
2026-05-02 12:25:38 +0800

    update post model

--- app/models/post.py
+++ app/models/post.py
@@ -1,4 +1,6 @@
-from sqlalchemy import String, Integer, ForeignKey
+from datetime import datetime, timezone
+
+from sqlalchemy import String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.models.base import Base
 
@@ -9,5 +11,10 @@ class Post(Base):
     id = mapped_column(Integer, primary_key=True)
     user_id = mapped_column(ForeignKey("users.id"))
     content = mapped_column(String(280))
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc)
+    )
 
     user = relationship("User", back_populates="posts")
